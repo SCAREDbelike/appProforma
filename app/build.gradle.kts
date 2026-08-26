2f539a0b35a175e1c9806737586c6dbc50f40d5f
Lia Malpartida <liamalpartida7@gmail.com>
2026-08-26 15:39:26 -0500

    Activity main arreglado, adición de item proforma y bg header para cambios visuales

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,18 @@ android {
 }
 
 dependencies {
+    // Solución al error de compilación: Forzar versión compatible con API 34
+    implementation("androidx.core:core-ktx:1.13.1")
 
-    implementation("androidx.appcompat:appcompat:1.8.0")
-    implementation("com.google.android.material:material:1.14.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.2")
+    // Componentes de interfaz
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Pruebas
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- app/src/main/java/com/jijijija/appproforma/MainActivity.java
+++ app/src/main/java/com/jijijija/appproforma/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jijijija.appproforma;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -8,7 +9,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Desactiva el modo oscuro para evitar cajas oscuras o texto ilegible
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
+
+        // Carga tu diseño de la proforma
         setContentView(R.layout.activity_main);
     }
 }
